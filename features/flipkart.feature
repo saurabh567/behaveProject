eef4ecb2d31d73f7afeadf89ac3da091779fb123
Saurav <saurav@gmail.com>
2023-06-05 17:20:14 +0530

    codechangeson5thjune2023

--- features/flipkart.feature
+++ features/flipkart.feature
@@ -3,5 +3,10 @@ Feature: Flipkart logo
     Given launch chrome browser
     When open the flipkart page
     Then verify the flipkart logo present on the page
-    Then close the browser
+    #Then close the browser
 
+Scenario: Presence on the flipkart page
+    Given launch chrome browser
+    When open the flipkart page
+    Then click on the popupclose button
+    Then click on the seller button
--- features/steps/flipkartlogopresent.py
+++ features/steps/flipkartlogopresent.py
@@ -1,3 +1,5 @@
+import time
+
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,20 +7,28 @@ from selenium.webdriver.common.by import By
 
 @given('launch chrome browser')
 def launchBrowser(context):
-    context.driver  = webdriver.Chrome(r"C:\Users\Get It Rent\Downloads\Software\chromedriver_win32")
+    context.driver  = webdriver.Chrome(r"C:\Users\Get It Rent\Downloads\Software\chromedriver_ver114")
 
     @when('open the flipkart page')
     def openFlipkartPage(context):
         context.driver.get("https://www.flipkart.com/")
+        context.driver.maximize_window()
 
         @then('verify the flipkart logo present on the page')
         def verifyFlipkartLogo(context):
-            status=context.driver.find_element(By.XPATH, "//img[@title='Flipkart']").is_displayed()
+            status=context.driver.find_element(By.XPATH, "//img1[@title='Flipkart']").is_displayed()
             assert status is True
 
             @then('close the browser')
             def closeBrowser(context):
                 context.driver.close()
+        @then('click on the popupclose button')
+        def popupClose(context):
+               time.sleep(5)
+               context.driver.find_element(By.XPATH, "//button[@class='_2KpZ6l _2doB4z']").click()
 
 
-#Author kumar Saurabh
+        @then('click on the seller button')
+        def clickOnCartButton(context):
+            time.sleep(10)
+            context.driver.find_element(By.XPATH, "//div[@class='go_DOp']//span[text()='Become a Seller']").click()

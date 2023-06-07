708e856632576a1f55f93c1ab8a0aa30ca9be955
Saurav <saurav@gmail.com>
2023-06-07 17:20:22 +0530

    codeupdatedon7june2023

--- features/steps/prodingerloginsteps.py
+++ features/steps/prodingerloginsteps.py
@@ -4,9 +4,11 @@ from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 @given('launch the chrome browser')
 def launchBrowser(context):
-    context.driver  = webdriver.Chrome(r"C:\Users\Get It Rent\Downloads\Software\chromedriver_win32")
+    context.driver = webdriver.Chrome(r"C:\Users\Get It Rent\Downloads\Software\chromedriver_win32")
+
 
 @when('open the prodinger page')
 def step_impl(context):
@@ -15,7 +17,7 @@ def step_impl(context):
 
 
 @then('enter the username "{user}" and password "{pwd}"')
-def step_impl(context,user,pwd):
+def step_impl(context, user, pwd):
     context.driver.find_element(By.XPATH, "(//input[@type='email'])[1]").send_keys(user)
     time.sleep(3)
     context.driver.find_element(By.XPATH, "(//input[@type='password'])[1]").send_keys(pwd)
@@ -25,17 +27,11 @@ def step_impl(context,user,pwd):
 def step_impl(context):
     context.driver.find_element(By.XPATH, "(//button[@type='submit'])[1]")
 
-#@then('user must successfully login  to the homepage')
-#def step_impl(context):
 
+# @then('user must successfully login  to the homepage')
+# def step_impl(context):
 
 
 @then('close the chrome browser')
 def closeBrowser(context):
     context.driver.close()
-
-
-
-
-
-

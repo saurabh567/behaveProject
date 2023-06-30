c8819e130452745bd9ce2228080466bf733ce4f8
Saurav <saurav@gmail.com>
2023-06-30 17:46:33 +0530

    testcasesadded

--- features/magentohomepage.feature
+++ features/magentohomepage.feature
@@ -1,40 +1,41 @@
 Feature: Magento login
 
-#  Scenario: Login in to Magento with valid parameters
-#    Given launch the chrome browser
-#    When open the magento luma page
-#    Then click on signin button
-#    Then enter the username "kumar.saurabh@pixelmechanics.de" and password "magento@1234"
-#    Then click on signin button of customer account page
-#    Then close the chrome browser
-#
-#
-#    Scenario Outline: Login in to Magento with Multiple parameters
-#    Given launch the chrome browser
-#    When open the magento luma page
-#    Then click on signin button
-#    Then enter the username "<username>" and password "<password>"
-#    Then close the chrome browser
-#
-#      Examples:
-#      |username                           | password    |
-#      |kumar.saurabh@pixelmechanics.de    |magento@1234 |
-#      |testkumar.saurabh@pixelmechanics.de| magento@1234|
-#
-#
-#
-#      Scenario: showcartbutton functionality
-#        Given launch the chrome browser
-#        When open the magento luma page
-#        Then click on the action showcart button
-#        Then get the title of the page
-#        Then click here link to continue shopping
-#
-#        Scenario: search functionality
-#        Given launch the chrome browser
-#        When open the magento luma page
-#        Then click and type "bags"inside the search bar
+  Scenario: Login in to Magento with valid parameters
+    Given launch the chrome browser
+    When open the magento luma page
+    Then click on signin button
+    Then enter the username "kumar.saurabh@pixelmechanics.de" and password "magento@1234"
+    Then click on signin button of customer account page
+    Then close the chrome browser
 
+
+    Scenario Outline: Login in to Magento with Multiple parameters
+    Given launch the chrome browser
+    When open the magento luma page
+    Then click on signin button
+    Then enter the username "<username>" and password "<password>"
+    Then close the chrome browser
+
+      Examples:
+      |username                           | password    |
+      |kumar.saurabh@pixelmechanics.de    |magento@1234 |
+      |testkumar.saurabh@pixelmechanics.de| magento@1234|
+
+
+
+      Scenario: showcartbutton functionality
+        Given launch the chrome browser
+        When open the magento luma page
+        Then click on the action showcart button
+        Then get the title of the page
+        Then click here link to continue shopping
+
+        Scenario: search functionality
+        Given launch the chrome browser
+        When open the magento luma page
+        Then click and type "bags"inside the search bar
+
+          #inprogress
         Scenario Outline: search functionality with multiple search terms
           Given launch the chrome browser
           When open the magento luma page
@@ -47,6 +48,42 @@ Feature: Magento login
            |bottoms      |
            |jackets      |
 
+   #inprogress
+  Scenario:handling header links by opening each of them in a separate tab
+   Given launch the chrome browser
+    When open the magento luma page
+    Then click on each of the header links to open them in a new tab
 
+
+  Scenario: Magento footer section links
+    Given launch the chrome browser
+    When open the magento luma page
+    Then click on the About us link
+    Then click on the Customer service link
+    Then click on the Privacy and cookie policy link
+    Then click on the Search Terms link
+    Then click on the Advanced Search link
+    Then click on the orders and Returns link
+    Then click on the Contact us link
+
+
+  Scenario: Hot sellers sections
+    Given launch the chrome browser
+    When open the magento luma page
+    #Then click on the first product image
+    Then click on each of the different sizes box individually
+    #Then navigate to the previous page
+    Then click on each of the colour swatches
+    Then click add to cart button
+    Then navigate to the counter quantity button to check the product has been added
+    Then click on proceed to checkout
+
+  Scenario: creating a new account
+    Given launch the chrome browser
+    When open the magento luma page
+    Then click on Create an account link
+    Then filling the personal information
+    Then filling the signin information
+    Then click on the create an account button
 
 
--- features/steps/magentohomepagesteps.py
+++ features/steps/magentohomepagesteps.py
@@ -1,5 +1,4 @@
 import time
-
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -70,5 +69,128 @@ def searchfunctionality(context):
     time.sleep(5)
 
 
+@then('click on each of the header links to open them in a new tab')
+def step_impl(context):
+    header_links = context.driver.find_elements(By.XPATH,"//li[contains(@class,'level0')]")
+    time.sleep(5)
+    for header_link in header_links:
+        header_link.click()
+        time.sleep(3)
+        context.driver.back()
+        time.sleep(10)
+
+
+@then('click on the About us link')
+def aboutUs(context):
+    context.driver.find_element(By.XPATH,"//a[contains(.,'About us')]").click()
+    time.sleep(5)
+    print(context.driver.current_url)
+    print(context.driver.title)
+    time.sleep(5)
+    context.driver.back()
+    time.sleep(5)
+
+
+@then('click on the Customer service link')
+def step_impl(context):
+    context.driver.find_element(By.XPATH,"//a[.='Customer Service']").click()
+    context.driver.back()
+    time.sleep(5)
+
+@then('click on the Privacy and cookie policy link')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//a[.='Privacy and Cookie Policy']").click()
+    context.driver.back()
+    time.sleep(5)
+
+@then('click on the Search Terms link')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//a[.='Search Terms']").click()
+    context.driver.back()
+    time.sleep(5)
+
+@then('click on the Advanced Search link')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//a[.='Advanced Search']").click()
+    context.driver.back()
+    time.sleep(5)
+    @then('click on the orders and Returns link')
+    def step_impl(context):
+        context.driver.find_element(By.XPATH, "//a[.='Orders and Returns']").click()
+        context.driver.back()
+        time.sleep(5)
+    @then('click on the Contact us link')
+    def step_impl(context):
+        context.driver.find_element(By.XPATH, "//a[.='Contact Us']").click()
+        context.driver.back()
+        time.sleep(5)
+
+# @then(u'click on the first product image')
+# def step_impl(context):
+#     context.driver.find_element(By.XPATH, "//li[contains(.,'Radiant Tee')]").click()
+
+
+@then('click on each of the different sizes box individually')
+def step_impl(context):
+    time.sleep(5)
+    context.driver.find_element(By.XPATH, "(//div[@role='listbox'])[1]//div[text()='XS']").click()
+    time.sleep(3)
+    context.driver.find_element(By.XPATH, "(//div[@aria-checked='false'])[2]").click()
+    context.driver.find_element(By.XPATH, "(//div[@aria-checked='false'])[3]").click()
+    context.driver.find_element(By.XPATH, "(//div[@aria-checked='false'])[4]").click()
+    context.driver.find_element(By.XPATH, "(//div[@aria-checked='false'])[5]").click()
+    time.sleep(2)
+
+# @then(u'navigate to the previous page')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Then navigate to the previous page')
+
+
+@then('click on each of the colour swatches')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//div[@aria-label='Blue']").click()
+    context.driver.find_element(By.XPATH, "//div[@aria-label='Orange']").click()
+    context.driver.find_element(By.XPATH, "(//div[@aria-label='Purple'])[1]").click()
+    time.sleep(3)
+
+
+@then('click add to cart button')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "(//span[contains(.,'Add to Cart')])[1]").click()
+    time.sleep(3)
+
+@then('navigate to the counter quantity button to check the product has been added')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//span[@class='counter-number']").click()
+    time.sleep(3)
+
+
+@then('click on proceed to checkout')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//button[.='Proceed to Checkout']").click()
+    time.sleep(3)
+
+@then('click on Create an account link')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//a[@id='idUZ4jec1F']").click()
+    time.sleep(3)
+
+
+@then('filling the personal information')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//input[@id='firstname']").send_keys("alok")
+    context.driver.find_element(By.XPATH, "//input[@id='lastname']").send_keys("sinha")
+    time.sleep(3)
+
+@then('filling the signin information')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//input[@title='Email']").send_keys("alok.kumar@pixelmechanics.de")
+    context.driver.find_element(By.XPATH, "//input[@id='password']").send_keys("sinha@123")
+    context.driver.find_element(By.XPATH, "//input[@id='password-confirmation']").send_keys("sinha@123")
+    time.sleep(3)
+@then('click on the create an account button')
+def step_impl(context):
+    context.driver.find_element(By.XPATH, "//span[.='Create an Account']").click()
+    time.sleep(5)
 
 
